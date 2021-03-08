a41c3bcc5a8e2636e25186606b63fbbb162e577e
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-03-08 12:43:14 +0800

    Update run.py

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def get_email ():#line:17
     OO00O00O0000O000O ='varytmp+{}uu{}d@gmail.com'.format (OO000O0000O0O0O00 ,OOOO0OOO00O0OO0O0 )#line:20
     return OO00O00O0000O000O #line:21
 def p_main (O00O0OOOO0O000000 ,OO000OO00OO00O00O ,OOOOOO000OO00O000 ):#line:22
-            OOOOO0OO0000OOOO0 ='https://pjj.one/share?userid='+OOOOOO000OO00O000 #line:23
+            OOOOO0OO0000OOOO0 ='https://jc88.me/share?userid='+OOOOOO000OO00O000 #line:23
             OO0OOOO000O0O000O =get_email ()#line:24
             print (OO0OOOO000O0O000O )#line:25
             O0O0OO00OO00O0O00 =Options ()#line:26
@@ -62,7 +62,7 @@ def p_main (O00O0OOOO0O000000 ,OO000OO00OO00O00O ,OOOOOO000OO00O000 ):#line:22
             time .sleep (2 )#line:88
             tx_code (O0O0O0000O0O0O00O )#line:89
             time .sleep (4 )#line:90
-            if O0O0O0000O0O0O00O .current_url =='https://pjj.one/home':#line:91
+            if O0O0O0000O0O0O00O .current_url =='https://jc88.me/home':#line:91
                 telegram_bot ("p++",'邀请成功！')#line:92
                 O0O0O0000O0O0O00O .quit ()#line:93
             else :#line:94
